5144843edd174edf64fc14cb70910def917c6813
G-Peña <89948235+g-pena@users.noreply.github.com>
2021-09-18 17:58:03 +0000

    if boiling or freezing

--- main.cs
+++ main.cs
@@ -2,6 +2,38 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+//variables
+double celsius;
+double farenheit;
+
+
+//Ask for temp. in Celsius
+Console.WriteLine("What is the temperature in Celsius?");
+celsius = Convert.ToDouble(Console.ReadLine());
+
+//Convert to Farenheit
+farenheit = (celsius*9/5)+32;
+Console.WriteLine("Farenheit equivalent for " + celsius);
+Console.WriteLine("degree celsius is " + farenheit);
+
+//Tell user if input is above boiling or below freezing
+
+if (farenheit <= 32)
+{
+  Console.WriteLine("This temperature is below freezing of 32 degrees Farenheit.");
+}
+
+if else (farenheit >= 212)
+{
+  Console.WriteLine("This temperatue is above boiling of 212 degrees Farenheit.");
+}
+
+
+
+
+
+
+
+
   }
 }
